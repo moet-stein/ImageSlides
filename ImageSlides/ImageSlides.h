48a4005af2126746f095c37f5700b699346e3195
moet-stein <0101.mste@gmail.com>
2022-06-13 17:34:49 +0200

    Initial Commit

---
+++ ImageSlides/ImageSlides.h
@@ -0,0 +1,18 @@
+//
+//  ImageSlides.h
+//  ImageSlides
+//
+//  Created by Moe Steinmueller on 13.06.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageSlides.
+FOUNDATION_EXPORT double ImageSlidesVersionNumber;
+
+//! Project version string for ImageSlides.
+FOUNDATION_EXPORT const unsigned char ImageSlidesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageSlides/PublicHeader.h>
+
+
